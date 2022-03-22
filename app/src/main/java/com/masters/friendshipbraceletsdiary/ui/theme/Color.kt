bbf963476401684cf0a36aead874dfd6bad218fc
Dimitrina Toleva <dimitrina.toleva@trading212.com>
2022-03-22 21:18:09 +0200

    update colors

--- app/src/main/java/com/masters/friendshipbraceletsdiary/ui/theme/Color.kt
+++ app/src/main/java/com/masters/friendshipbraceletsdiary/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.masters.friendshipbraceletsdiary.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val ColorPrimary = Color(0xFFBFFFF0)
+val ColorSecondary = Color(0xFFF0FFC2)
+val ColorAccent = Color(0xFFFFBBBB)
+val ColorDescription = Color(0xFFFFE4C0)
+val ColorBackground = Color(0xFFEDFFEC)
--- app/src/main/java/com/masters/friendshipbraceletsdiary/ui/theme/Theme.kt
+++ app/src/main/java/com/masters/friendshipbraceletsdiary/ui/theme/Theme.kt
@@ -5,17 +5,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+import androidx.compose.ui.graphics.Color
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = ColorPrimary,
+    primaryVariant = ColorAccent,
+    secondary = ColorSecondary,
+    background = ColorBackground,
+    onPrimary = Color.Black,
 
     /* Other default colors to override
     background = Color.White,
@@ -30,16 +27,8 @@ private val LightColorPalette = lightColors(
 @Composable
 fun FriendshipBraceletsDiaryTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit)
 {
-    val colors = if (darkTheme)
-    {
-        DarkColorPalette
-    } else
-    {
-        LightColorPalette
-    }
-
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
